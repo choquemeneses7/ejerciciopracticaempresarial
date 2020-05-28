8b7c8c20a2d5af86aaad9c3578b01535a01f6c77
Veimar Choque <veimar.choque@truextend.com>
2020-05-28 16:56:32 -0400

    Feature-Optimization: Improved the home component to show error messages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser'; 
 import data from '../../../data.json';
-import * as config from '../../../config.json';
+import * as config from '../../../labels.json';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +11,7 @@ export class HomeComponent implements OnInit {
   
   readonly users = (<any>data).users;
   readonly title : string = (<any>config).appName;
+  readonly errormessage : string = (<any>config).labelNoUsers;
   
   constructor(title:Title)
   {
